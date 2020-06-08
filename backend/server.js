5c82adc478b59e5d553211fae48b769f18360bac
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-08 13:24:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
+//import dotenv from "dotenv";
 import path from "path";
 import userRoute from "./Route/Route";
 const bodyParser = require("body-parser");
 import productRoute from "./Route/productRoute";
+import config from "./config";
 const mongoose = require("mongoose");
 const express = require("express");
+//dotenv.config();
 
 const mongodbURL = process.env.MONGODB_URL;
+console.log(mongodbURL);
 mongoose.set("useCreateIndex", true);
 mongoose
   .connect(mongodbURL, {
@@ -17,6 +21,7 @@ mongoose
 
 const app = express();
 app.use(bodyParser.json());
+//app.use(favicon(path.join(dirname, "build", "favicon.ico")));
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
